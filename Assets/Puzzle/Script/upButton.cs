4ce13c5c075614cad99e7c558867ce863dacc067
tucker87 <jtgeek@gmail.com>
2014-03-02 16:40:37 -0500

    First functional script!
    Up Button rotates the work Selector 90 degrees.

---
+++ Assets/Puzzle/Script/upButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class upButton : MonoBehaviour {
+
+	public float speed = 0.1f;
+	public float targetAngle = 0;
+	public int step = 90;
+	private bool rotating = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject WordSelect = GameObject.Find ("WordSelect1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject WordSelect = GameObject.Find ("WordSelect1");
+		//float tiltAroundX = Input.GetAxis ("Vertical") * step;
+		float tiltAroundX = targetAngle;
+		Quaternion target = Quaternion.Euler (tiltAroundX, 0, 90);
+		WordSelect.transform.rotation = Quaternion.Slerp (WordSelect.transform.rotation, target, Time.deltaTime * speed);
+	}
+
+	void OnMouseUpAsButton() {
+		//When button is pressed WordSelect rotates to reveal another word.
+		targetAngle = targetAngle + step;
+	}
+}
