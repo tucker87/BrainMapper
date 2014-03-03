81654de70fa86590635e345c6b850b5f0df181df
tucker87 <jtgeek@gmail.com>
2014-03-02 23:53:08 -0500

    First Object!
    Word Selectors now load text and rotate at proper steps. (120 degrees
    when there are 3 words etc.)

--- Assets/Puzzle/Script/upButton.cs
+++ Assets/Puzzle/Script/upButton.cs
@@ -3,29 +3,22 @@ using System.Collections;
 
 public class upButton : MonoBehaviour {
 
-	public float speed = 0.1f;
-	public float targetAngle = 0;
-	public int step = 90;
-	private bool rotating = false;
-
+	//Which wordSelect Object this button controls.
+	public int controls;
 
 
 	// Use this for initialization
 	void Start () {
-		GameObject WordSelect = GameObject.Find ("WordSelect1");
+		GameObject[] WordSelect = GameObject.FindGameObjectsWithTag("WordSelector");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GameObject WordSelect = GameObject.Find ("WordSelect1");
-		//float tiltAroundX = Input.GetAxis ("Vertical") * step;
-		float tiltAroundX = targetAngle;
-		Quaternion target = Quaternion.Euler (tiltAroundX, 0, 90);
-		WordSelect.transform.rotation = Quaternion.Slerp (WordSelect.transform.rotation, target, Time.deltaTime * speed);
+
 	}
 
 	void OnMouseUpAsButton() {
-		//When button is pressed WordSelect rotates to reveal another word.
-		targetAngle = targetAngle + step;
+
 	}
 }
--- Assets/Puzzle/Script/wordSelect.cs
+++ Assets/Puzzle/Script/wordSelect.cs
@@ -3,37 +3,49 @@ using System.Collections;
 
 public class wordSelect : MonoBehaviour {
 
-	void Start() {
-		print ("Start");
-		TextMesh Word1 = (TextMesh)this.GetComponentInChildren (typeof(TextMesh));
-		Word1.text = "Test";
+	//Set in Unity properties to determine which wordList is used.
+	public int wordOrder;
+	//Variables for rotation
+	private float speed;
+	private float targetAngle;
+	private int step;
+
+	void OnGUI() {
+		//Gets objects position on the screen
+		Vector3 screenPosition = Camera.main.WorldToScreenPoint (this.transform.position);
+
+		//When button is pressed WordSelect rotates to reveal another word.
+		if (GUI.Button (new Rect (screenPosition.x - 75, screenPosition.y - 125, 150, 50), "Test")) {
+			targetAngle = targetAngle + step;
+		}
+		
 	}
 
-	int speed;
-	float friction, lerpSpeed;
-	private float xDeg;
-	private float yDeg;
-	private Quaternion fromRotation;
-	private Quaternion toRotation;
+	void Start() {
+		speed = 4;
+		targetAngle = 0;
 
-	
+		//Word set will be defined be menus later. Hard coding for now.
+		string[,] wordList = { { "This", "That", "The" }, { "is", "are", "was" }, { "Silly", "War", "Sparta" } };
+		TextMesh[] wordObjects = (TextMesh[])GetComponentsInChildren<TextMesh>();
+		step = 360 / wordObjects.Length;
+
+		for (int i = 0; i < wordObjects.Length; i++) {
+			wordObjects[i].text = wordList[wordOrder,i];
+
+			//Attempt to force rotation around cylinder rather then repoitioning.
+			Vector3 rotationMask = new Vector3(1,0,0);
+			//wordObjects[i].transform.RotateAround(transform.position, rotationMask, step * Time.deltaTime);
+			print(step);
+			wordObjects[i].transform.RotateAround(transform.position, rotationMask, step * i);
+
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-//		Vector3 mousePosition = Input.mousePosition;
-//		Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
-//		float targetPosition = Mathf.Clamp (worldMousePosition.y,-360,360);
-//		Vector3 currentPosition = transform.rotation;
-//		currentPosition.y = Mathf.MoveTowards (currentPosition.y, targetPosition, maxUnitsPerSecond);
-//		transform.rotation = currentPosition;
-		lerpSpeed = 1;
-
-		if (Input.GetMouseButton (0)) {
-			xDeg -= Input.GetAxis ("Mouse X");
-			yDeg += Input.GetAxis ("Mouse Y");
-			fromRotation = transform.rotation;
-			toRotation = Quaternion.Euler (yDeg, 0, 0);
-			transform.Rotate(5,0,0,Space.World);
-			//transform.Rotate = Quaternion.Lerp (fromRotation, toRotation, Time.deltaTime * lerpSpeed);
-		}
+		//Rotates cylinder to new angle
+		float tiltAroundX = targetAngle;
+		Quaternion target = Quaternion.Euler (tiltAroundX, 0, 90);
+		transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * speed);
 	}
 }
